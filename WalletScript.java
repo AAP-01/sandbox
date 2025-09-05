e2ddb1b09ab72789c5e7c207ec08b546f9cb3fa8
John Benedict Castroverde <jbthememer43@gmail.com>
2025-09-05 13:02:55 -0400

    added a method for WalletScript printing how many silver coins the user has in their wallet using a conditional statement

--- WalletScript.java
+++ WalletScript.java
@@ -9,7 +9,19 @@ public class WalletScript
 {
     public WalletScript(int silver_coin, int gold_coin)
     {
-        
+        Silver_Coin = silver_coin;
     }
     
+    private int Silver_Coin;
+    
+    public void showWalletContents()
+    //this is a method
+    {
+        if (Silver_Coin > 1){
+        System.out.println("You have " + Silver_Coin + " silver coins");
+        }
+        else{
+        System.out.println("You have " + Silver_Coin + " silver coin");
+        }
+    }
 }
